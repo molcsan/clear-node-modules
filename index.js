5944285cc7525892307bd08159a555d807c0a8ab
Molcsan <p.molcsan@gmail.com>
2022-10-25 14:33:14 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const currentDir = __dirname;
+const nodeModulesDir = path.resolve(currentDir, 'node_modules');
+
+//Delete the node_modules folder
+if (fs.existsSync(nodeModulesDir)) {
+    fs.rm(nodeModulesDir, { recursive: true, force: true }, err => {
+        if (err) {
+            throw err
+        }
+
+        console.log(`${dir} is deleted!`)
+    })
+}
